e06c3e8099b59c6e166e5c1985113b8ef5a80b09
ess2 <ess2@cin.ufpe.br>
2019-09-15 19:42:43 -0300

    Legibility changes

--- Bioinspirada/MiniProjeto1/bin/8QueensAlgorithm.py
+++ Bioinspirada/MiniProjeto1/bin/8QueensAlgorithm.py
@@ -1,6 +1,75 @@
 import numpy
 import random
 
+def main():
+    population = list()
+    qtFitness = 0
+
+    #Gera a população
+    for i in range(100):
+        a = random.sample(range(1,9), 8)
+        if a not in population:
+            fitn = fitness(a)
+            qtFitness += 1
+            tuple = (a, fitn)
+            population.append(tuple)
+            binaryArray = ['', '', '', '', '', '', '', '']
+            b = numpy.array(tuple[0])
+            for u in range(8):
+                binaryArray[u] = '{0:03b}'.format(b[u])
+            print "Genótipo " + str(i) + ":" + str(binaryArray)
+        else:
+            i = i - 1
+
+    population.sort(key=takeSecond,reverse=True)
+
+    firstElementList = population[0]
+
+    while ((population[0])[1] < 28 and qtFitness < 10000):
+        probRecombNumber = random.randint(1,10)
+        probMutNumber = random.randint(1,10)
+
+        if (probRecombNumber > 0) and (probRecombNumber < 10):
+            firstEl = population[0]
+            secondEl = population[1]
+            childs = reproduce(firstEl[0], secondEl[0])
+            fitness1 = fitness(childs[0])
+            fitness2 = fitness(childs[1])
+            qtFitness += 2
+            tuple = (childs[0], fitness1)
+            tuple2 = (childs[1], fitness2)
+            population = excludeBad(population, [tuple, tuple2])
+            population.sort(key=takeSecond, reverse=True)
+
+        if(probMutNumber > 0) and (probMutNumber < 5):
+            firstEl = population[0]
+            child = mutate(firstEl[0])
+            fitn = fitness(child)
+            qtFitness += 1
+            tuple = (child, fitn)
+            population = excludeBad(population, [tuple])
+            population.sort(key=takeSecond, reverse=True)
+
+        firstElementList = population[0]
+
+
+    binaryArray = ['','','','','','','','']
+    print "Melhor Solução:"
+    element = population[0]
+
+    a = numpy.array(element[0])
+
+    for i in range(8):
+        binaryArray[i] = '{0:03b}'.format(a[i])
+
+    print binaryArray
+
+    print "Fitness:"
+    print (population[0])[1]
+
+    print "Número de avaliações de fitness:"
+    print qtFitness
+
 def fitness(individual):
     clashes = 0;
     # calculate row and column clashes
@@ -36,59 +105,50 @@ def mutate(x):
 def reproduce(x, y):
     n = len(x)
     c = random.randint(0, n - 1)
-    tx = x[0:(c+1)]
-    ty = y[0:(c+1)]
+    childX = x[0:(c+1)]
+    childY = y[0:(c+1)]
     d = c + 1
 
-    if d==n:
+    if d==n or d==(n-1):
         return (x, y)
     else:
         for k in range(d,n):
-            tempX = x[k]
+            #Generate new elements for Child Y
+            count = k
+            for i in range(k,n):
+                tempX = x[i]
 
-            if tempX not in ty:
-                ty += [tempX]
-            else:
-                count = k
-                for i in range(k,n):
-                    tempX = x[i]
-
-                    if tempX not in ty:
-                        ty+=[tempX]
-                        count += 1
-                        break
+                if tempX not in childY:
+                    childY+=[tempX]
                     count += 1
+                    break
+                count += 1
 
-                if(count == n):
-                    for j in range(c+1):
-                        tempX = x[j]
-                        if tempX not in ty:
-                            ty += [tempX]
-                            break
-
-            #Trocando o array X
-            tempY = y[k]
-
-            if tempY not in tx:
-                tx += [tempY]
-            else:
-                count = k
-                for i in range(k,n):
-                    tempY = y[i]
-
-                    if tempY not in tx:
-                        tx+=[tempY]
-                        count += 1
+            if(count == n):
+                for j in range(c+1):
+                    tempX = x[j]
+                    if tempX not in childY:
+                        childY += [tempX]
                         break
-                    count += 1
 
-                if(count == n):
-                    for j in range(c+1):
-                        tempY = y[j]
-                        if tempY not in tx:
-                            tx += [tempY]
-                            break
-    return (tx, ty)
+            # Generate new elements for Child X
+            count = k
+            for i in range(k,n):
+                tempY = y[i]
+
+                if tempY not in childX:
+                    childX+=[tempY]
+                    count += 1
+                    break
+                count += 1
+
+            if(count == n):
+                for j in range(c+1):
+                    tempY = y[j]
+                    if tempY not in childX:
+                        childX += [tempY]
+                        break
+    return (childX, childY)
 
 def excludeBad(finalList, elements):
     for i in range(len(elements)):
@@ -99,72 +159,5 @@ def excludeBad(finalList, elements):
 def takeSecond(elem):
     return elem[1]
 
-popl = list()
-listPositions = list()
-qtFitness = 0
-
-#Gera a população
-for i in range(100):
-    a = random.sample(range(1,9), 8)
-    if a not in listPositions:
-        fitn = fitness(a)
-        qtFitness += 1
-        tuple = (a, fitn)
-        listPositions.append(tuple)
-        binaryArray = ['', '', '', '', '', '', '', '']
-        b = numpy.array(tuple[0])
-        for u in range(8):
-            binaryArray[u] = '{0:03b}'.format(b[u])
-        print "Genótipo " + str(i) + ":" + str(binaryArray)
-    else:
-        i = i - 1
-
-print len(listPositions)
-listPositions.sort(key=takeSecond,reverse=True)
-
-firstElementList = listPositions[0]
-
-while ((listPositions[0])[1] < 28 and qtFitness < 10000):
-    probRecombNumber = random.randint(1,10)
-    probMutNumber = random.randint(1,10)
-
-    if (probRecombNumber > 0) and (probRecombNumber < 10):
-        firstEl = listPositions[0]
-        secondEl = listPositions[1]
-        childs = reproduce(firstEl[0], secondEl[0])
-        fitness1 = fitness(childs[0])
-        fitness2 = fitness(childs[1])
-        qtFitness += 2
-        tuple = (childs[0], fitness1)
-        tuple2 = (childs[1], fitness2)
-        listPositions = excludeBad(listPositions, [tuple, tuple2])
-        listPositions.sort(key=takeSecond, reverse=True)
-
-    if(probMutNumber > 0) and (probMutNumber < 5):
-        firstEl = listPositions[0]
-        child = mutate(firstEl[0])
-        fitn = fitness(child)
-        qtFitness += 1
-        tuple = (child, fitn)
-        listPositions = excludeBad(listPositions, [tuple])
-        listPositions.sort(key=takeSecond, reverse=True)
-
-    firstElementList = listPositions[0]
-
-
-binaryArray = ['','','','','','','','']
-print "Melhor Solução:"
-element = listPositions[0]
-
-a = numpy.array(element[0])
-
-for i in range(8):
-    binaryArray[i] = '{0:03b}'.format(a[i])
-
-print binaryArray
-
-print "Fitness:"
-print (listPositions[0])[1]
-
-print "Número de avaliações de fitness:"
-print qtFitness
+if __name__ == '__main__':
+    main()
